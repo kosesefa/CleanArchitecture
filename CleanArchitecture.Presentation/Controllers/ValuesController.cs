e98d35be6a62be1295ae1e695af7a800866947c3
Sefa Köse <sefakose23@gmail.com>
2023-05-20 23:00:13 +0300

    Add project files.

---
+++ CleanArchitecture.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class ValuesController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Deneme işlemi başarılı.");
+        }
+    }
+}
