051232bd60daeb6488eeff12439f956ed26f0c9b
Sefa Köse <sefakose23@gmail.com>
2023-05-23 20:34:15 +0300

    Mapping,validation and exception middleware done.

---
+++ CleanArchitecture.Application/Features/CarFeatures/Commands/CreateCar/CreateCarCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar
+{
+    public sealed class CreateCarCommandValidator:AbstractValidator<CreateCarCommand>
+    {
+        public CreateCarCommandValidator() 
+        {
+            //Name
+            RuleFor(p=>p.Name).NotEmpty().WithMessage("Araç adı boş olamaz !");
+            RuleFor(p => p.Name).NotNull().WithMessage("Araç adı boş olamaz !");
+            RuleFor(p => p.Name).MinimumLength(3).WithMessage("Araç adı 3 karakterden az olamaz !");
+
+            //Model
+            RuleFor(p => p.Model).NotEmpty().WithMessage("Araç modeli boş olamaz !");
+            RuleFor(p => p.Model).NotNull().WithMessage("Araç modeli boş olamaz !");
+            RuleFor(p => p.Model).MinimumLength(3).WithMessage("Araç modeli en az 3 karakter olmalıdır !");
+
+            //EnginePower
+            RuleFor(p => p.EnginePower).NotEmpty().WithMessage("Araç motor gücü boş bırakılamaz!");
+            RuleFor(p => p.EnginePower).NotEmpty().WithMessage("Araç motor gücü boş olamaz !");
+            RuleFor(p => p.EnginePower).GreaterThan(0).WithMessage("Araç motor gücü 0'dan küçük olamaz !");
+        }
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,12 +1,19 @@
+using CleanArchitecture.Application.Behaviors;
 using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistance.Context;
 using CleanArchitecture.Persistance.Services;
+using CleanArchitecture.WebApi.Middleware;
+using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ICarService, CarService>();
+builder.Services.AddTransient<ExceptionMiddleware>();
+
+builder.Services.AddAutoMapper(typeof(CleanArchitecture.Persistance.AssemblyReference).Assembly);
 
 string connectionString = builder.Configuration.GetConnectionString("SqlServer");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
@@ -17,6 +24,8 @@ builder.Services.AddControllers()
 builder.Services.AddMediatR(cfr =>
 cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly));
 
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>),typeof(ValidationBehavior<,>));
+builder.Services.AddValidatorsFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddlewareExtensions();
 
 app.UseHttpsRedirection();
 
