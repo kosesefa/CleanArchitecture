d5d6d7b6b549b5484221831bc27f38136b76415d
Sefa Köse <sefakose23@gmail.com>
2023-06-06 16:05:38 +0300

    Create token method added

---
+++ CleanArchitecture.Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Abstractions
+{
+    public interface IJwtProvider
+    {
+        string CreateToken(User user);
+    }
+}
